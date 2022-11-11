c9a7c88f10fb8547613820d2f72a76e114ab9f86
asya-code <77514066+asya-code@users.noreply.github.com>
2022-11-11 09:12:29 -0800

    inherritance, overriding and polymorphism exercises

---
+++ models/Shirt.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.util.Objects;
+
+public class Shirt extends Product{
+    private String size;
+
+    public Shirt(String size, double price, String color, String brand) {
+        super(price, color, brand);
+        this.size = size;
+    }
+
+    public Shirt(Shirt source) {
+        super(source);
+        this.size = source.size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shirt shirt = (Shirt) o;
+        return size.equals(shirt.size)
+                && super.getPrice() == shirt.getPrice()
+                && super.getColor().equals(shirt.getColor())
+                && super.getBrand().equals(shirt.getBrand());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, super.getPrice(), super.getColor(), super.getBrand());
+    }
+}
